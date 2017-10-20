84d343327d84e9aa373d96fbdc777c047a80ea0c
helderpereiramartins <up201404891@fe.up.pt>
2017-10-20 18:19:42 +0100

    Versão com limitação na entrada de comandos (quase)

--- Here Be Dragons/src/Game.java
+++ Here Be Dragons/src/Game.java
@@ -10,18 +10,25 @@ public class Game {
 	private static final String KEY_DOWN = "s";
 	private static final String KEY_RIGHT = "d";
 	private static final String KEY_LEFT = "a";
+	private static final String KEY_STILL = "p";
+	private static final String INVALID = "invalido";
 
 	private int NumDragons = 10;
 
 	Movimento movimentoObject = new Movimento();
 
-	/*Questiona o numero de dragões ao utilizador e retorna o Int com o valor
-	 * System.out.print("\n"); é usado pois na iteração feita a seguir o comand.nextLine 
-	 * faz skip pois aqui é feito um nextInt*/
+	/*
+	 * Questiona o numero de dragões ao utilizador e retorna o Int com o valor
+	 * System.out.print("\n"); é usado pois na iteração feita a seguir o
+	 * comand.nextLine faz skip pois aqui é feito um nextInt
+	 */
 	public int Dificulty() {
 		System.out.print("Quantos Dragões quer? (max 5) ");
 		while (NumDragons > 5) {
 			NumDragons = comand.nextInt();
+			if (NumDragons > 5){
+				System.out.print("Numero maximo de dragões permitidos é 5\n\nInsira um numero válido: ");
+			}
 		}
 		System.out.print("\n");
 		comand.nextLine();
@@ -58,10 +65,22 @@ public class Game {
 			case KEY_LEFT:
 				c = -1;
 				break;
+
+			case KEY_STILL:
+				break;
+
+			default:
+				comando = INVALID;
 			}
 
-			break;
+			if (comando != INVALID) {
+				break;
+			} else {
+				System.out.print("Insira um comando válido (w,a,s,d,p)");
+			}
+
 		}
+
 		movimentoObject.movement(l, c);
 
 	}
